53263806abe31546cbe6ce97a1bdb65c00d66098
tafaramafemba <tafara.maf@gmail.com>
2022-04-21 18:28:14 +0200

    implement functional component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,22 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
+export default function Calculator() {
+  const [defaultState, changeState] = useState({ total: null, next: null, operation: null })
 
-  }
-
-  onClickEvent = (event) => {
+  const onClickEvent = (event) => {
     const { value } = event.target;
-    const { total, next, operation } = calculate(this.state, value);
-    this.setState({ total, next, operation });
+    const { total, next, operation } = calculate(defaultState, value);
+    changeState({ total, next, operation });
     const output = document.querySelector('.input');
     if (next !== null) {
       output.innerHTML = next;
@@ -27,47 +19,43 @@ class Calculator extends React.Component {
     }
   };
 
-  render() {
-    return (
-      <div className="calculator">
-        <div className="input-div">
+  return (
+    <div className="calculator">
+      <div className="input-div">
         <h2 className="input">0</h2>
-        </div>
-
-        <div className="row">
-          <button onClick={this.onClickEvent} value="AC" className="grey-btn" type="button">AC</button>
-          <button onClick={this.onClickEvent} value="+/-" className="grey-btn" type="button">+/-</button>
-          <button onClick={this.onClickEvent} value="%" className="grey-btn" type="button">%</button>
-          <button onClick={this.onClickEvent} value="÷" className="orange-btn" type="button">÷</button>
-        </div>
-        <div className="row">
-          <button onClick={this.onClickEvent} value="7" className="grey-btn" type="button">7</button>
-          <button onClick={this.onClickEvent} value="8" className="grey-btn" type="button">8</button>
-          <button onClick={this.onClickEvent} value="9" className="grey-btn" type="button">9</button>
-          <button onClick={this.onClickEvent} value="x" className="orange-btn" type="button">x</button>
-        </div>
-        <div className="row">
-          <button onClick={this.onClickEvent} value="4" className="grey-btn" type="button">4</button>
-          <button onClick={this.onClickEvent} value="5" className="grey-btn" type="button">5</button>
-          <button onClick={this.onClickEvent} value="6" className="grey-btn" type="button">6</button>
-          <button onClick={this.onClickEvent} value="-" className="orange-btn" type="button">-</button>
-        </div>
-        <div className="row">
-          <button onClick={this.onClickEvent} value="1" className="grey-btn" type="button">1</button>
-          <button onClick={this.onClickEvent} value="2" className="grey-btn" type="button">2</button>
-          <button onClick={this.onClickEvent} value="3" className="grey-btn" type="button">3</button>
-          <button onClick={this.onClickEvent} value="+" className="orange-btn" type="button">+</button>
-        </div>
-        <div className="row">
-          <button onClick={this.onClickEvent} value="0" className="grey-btn plus" type="button">0</button>
-          <button onClick={this.onClickEvent} value="." className="grey-btn" type="button">.</button>
-          <button onClick={this.onClickEvent} value="=" className="orange-btn" type="button">=</button>
-        </div>
-
       </div>
 
-    );
-  }
-}
+      <div className="row">
+        <button onClick={onClickEvent} value="AC" className="grey-btn" type="button">AC</button>
+        <button onClick={onClickEvent} value="+/-" className="grey-btn" type="button">+/-</button>
+        <button onClick={onClickEvent} value="%" className="grey-btn" type="button">%</button>
+        <button onClick={onClickEvent} value="÷" className="orange-btn" type="button">÷</button>
+      </div>
+      <div className="row">
+        <button onClick={onClickEvent} value="7" className="grey-btn" type="button">7</button>
+        <button onClick={onClickEvent} value="8" className="grey-btn" type="button">8</button>
+        <button onClick={onClickEvent} value="9" className="grey-btn" type="button">9</button>
+        <button onClick={onClickEvent} value="x" className="orange-btn" type="button">x</button>
+      </div>
+      <div className="row">
+        <button onClick={onClickEvent} value="4" className="grey-btn" type="button">4</button>
+        <button onClick={onClickEvent} value="5" className="grey-btn" type="button">5</button>
+        <button onClick={onClickEvent} value="6" className="grey-btn" type="button">6</button>
+        <button onClick={onClickEvent} value="-" className="orange-btn" type="button">-</button>
+      </div>
+      <div className="row">
+        <button onClick={onClickEvent} value="1" className="grey-btn" type="button">1</button>
+        <button onClick={onClickEvent} value="2" className="grey-btn" type="button">2</button>
+        <button onClick={onClickEvent} value="3" className="grey-btn" type="button">3</button>
+        <button onClick={onClickEvent} value="+" className="orange-btn" type="button">+</button>
+      </div>
+      <div className="row">
+        <button onClick={onClickEvent} value="0" className="grey-btn plus" type="button">0</button>
+        <button onClick={onClickEvent} value="." className="grey-btn" type="button">.</button>
+        <button onClick={onClickEvent} value="=" className="orange-btn" type="button">=</button>
+      </div>
 
-export default Calculator;
+    </div>
+
+  );
+}
