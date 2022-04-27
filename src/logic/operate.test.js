bdbd3041631537dfd1a2dfba0836cd3c13a63604
tafaramafemba <tafara.maf@gmail.com>
2022-04-27 22:39:06 +0200

    calculate and operate tests

---
+++ src/logic/operate.test.js
@@ -0,0 +1,29 @@
+import operate from './operate';
+
+describe('Testing calculator operations', () => {
+  test('Plus operand', () => {
+
+expect(operate(1, 2, '+')).toBe('3');
+  });
+
+  test('Minus operand', () => {
+
+  expect(operate(3, 2, '-')).toBe('1');
+  });
+
+  test('Times operand', () => {
+
+    expect(operate(3, 2, 'x')).toBe('6');
+    });
+
+    test('Divide operand', () => {
+
+      expect(operate(6, 2, '÷')).toBe('3');
+      });
+
+      test('Modulo operand', () => {
+
+        expect(operate(7, 2, '%')).toBe('1');
+        });
+
+});
