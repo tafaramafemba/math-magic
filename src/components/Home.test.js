b0c76a01f2ab1b1dd62c3c53ecda776218bf00f6
isaacpitwa <isaacpitwa256@gmail.com>
2022-04-28 00:17:24 +0300

    Home Component tests

--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -1,5 +1,10 @@
-describe('Home', () => {
-    test('Placeholder', () => {
-    expect(1+1).toBe(2)
-    })
-  })
+import renderer from 'react-test-renderer'
+import Home from "./Home";
+
+describe(' Home Component Rendering ', () =>{
+    it('renders correctly', () => {
+        const tree = renderer
+            .create(<Home />)
+        expect(tree).toMatchSnapshot();
+    });
+})
